fd71ccaf91177ec546eca8fd7b2c25e47e3a32ce
JMW_1999 <54145971+Zero-coder@users.noreply.github.com>
2026-06-26 10:52:24 +0800

    Add BrainFuse package exports

---
+++ brainfuse/__init__.py
@@ -0,0 +1,6 @@
+"""BrainFuse-LLM core modules."""
+
+from brainfuse.models.neuro_encoder import NeuroEnhancedEncoder
+from brainfuse.models.neuro_lora_moe import NeuroLoRAMoELinear
+
+__all__ = ["NeuroEnhancedEncoder", "NeuroLoRAMoELinear"]
